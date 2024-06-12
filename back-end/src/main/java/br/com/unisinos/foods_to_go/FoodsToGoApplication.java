90922c1fb2610810ae2f8fab7d68014fcc1b142c
breno-ms <breno.wrk@gmail.com>
2024-06-12 20:40:03 -0300

    feat: initializing the project

---
+++ back-end/src/main/java/br/com/unisinos/foods_to_go/FoodsToGoApplication.java
@@ -0,0 +1,13 @@
+package br.com.unisinos.foods_to_go;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodsToGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodsToGoApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/br/com/unisinos/foods_to_go/FoodsToGoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unisinos.foods_to_go;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodsToGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
